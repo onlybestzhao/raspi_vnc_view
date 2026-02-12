78128e4d115f6ff0d0d63f1ac5020b490e7e8948
onlybestzhao <1590257224@qq.com>
2026-02-12 20:54:09 +0000

    README

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
-# 
+# 一、概述
+- camera:raspi_ov5647
+- raspi 4B
+- python3 :3.11
+- opencv : 4.11.0.86
+- raspi+opencv 
+- os rapsi 64位 最小系统，无桌面显示
+- 灰度和边缘检测，非常ez的一个小demo，只是做一个实时传到vnc上的测试
+- 后期准备加深度学习部署AI算法
--- test_vnc_view.py
+++ test_vnc_view.py
@@ -13,7 +13,7 @@ picam2.start()
 
 WINDOW_NAME = "raspi_实时处理画面"
 cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)  # 允许窗口缩放
-cv2.resizeWindow(WINDOW_NAME, 1280, 720)
+cv2.resizeWindow(WINDOW_NAME, 1280, 1020)
 try:
     print("开始实时显示，按 'q' 键退出...")
     while True:
